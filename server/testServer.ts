45aac902dc2f4658a94b2492ec7f55f089d3a837
ichir0roieHome <ichir0roie.com>
2022-03-23 23:46:17 +0900

    move server

---
+++ server/testServer.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+const addr = ":8085";
+console.log(`HTTP server listening on http://localhost${addr}`)
+
+serve((request: Request) => {
+  const { pathname } = new URL(request.url);
+
+  return new Response(`Hello world from ${pathname}`);
+}, { addr });
