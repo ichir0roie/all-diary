713a66da073b02109fc4e4095f5d473e3a776463
ichir0roieHome <ichir0roie.com>
2022-05-22 21:59:57 +0900

    relative!!

--- ui-test-windows/components/card.tsx
+++ ui-test-windows/components/card.tsx
@@ -4,6 +4,8 @@ import {useState} from 'react'
 //なんと！"key"は予約語かなんかなのか！？
 export interface CardProps{
   testKey:String;
+  posX:number;
+  posY:number;
 }
 
 export default function Card(props:CardProps) {
@@ -12,7 +14,7 @@ export default function Card(props:CardProps) {
   return (
     // <div className="card-frame" key={props.testKey.toString()}>個々でやっても意味なかった。
     // https://dev.classmethod.jp/articles/avoiding-warningeach-child-in-a-list-should-have-a-unique-key-prop-in-react-apps-is-called-and-not-on-the-side-do-it-on-the-caller/
-      <div className="card-frame card">
+      <div className="card" style={{top:props.posX ,left:props.posY} }>
           <p>title</p>
           <p>text area</p>
           <p>{props.testKey}</p>
--- ui-test-windows/pages/index.tsx
+++ ui-test-windows/pages/index.tsx
@@ -20,9 +20,37 @@ function get2dKeys(lenRow: number, lenCol: number) {
 
 export default function Home() {
   const [dataArray, setDataArray] = useState(get2dKeys(20, 20));
+  
+  const[boxX,setBoxX]=useState(0);
+  const[boxY,setBoxY]=useState(0);
+  const[catchX,setCatchX]=useState(0);
+  const[catchY,setCatchY]=useState(0);
 
+  var cardsTemp:JSX.Element[]=[]
+  
   console.log(dataArray);
 
+  function onScrollInBox(ev:React.UIEvent<HTMLDivElement>){
+    setBoxX(ev.currentTarget.clientWidth);
+    setBoxY(ev.currentTarget.clientHeight);
+    setCatchX(ev.currentTarget.scrollLeft);
+    setCatchY(ev.currentTarget.scrollTop);
+    // console.log(ev);
+    newCard(Math.floor(Math.random() * 500),Math.floor(Math.random() * 500));
+  }
+  
+  function newCard(posX:number,posY:number){
+    const nc=<Card testKey={posX.toString()+":"+posY.toString()} posX={posX} posY={posY} />;
+    cardsTemp.push(nc);
+    return nc;
+  }
+  newCard(0,0);
+  newCard(50,0);
+  newCard(0,50);
+  newCard(100,100);
+  
+  // const[cards , setCards]=useState<JSX.Element[]>(cardsTemp);
+  
   return (
     <div className="page">
       {
@@ -34,11 +62,18 @@ export default function Home() {
         </tr>
       </table> */
       }
-      <div className="scrollBox">
-        <div className="scrollItem">
-          <CardArray keyArray={dataArray} key="unique" />
-        </div>
+      <div className="scrollBox" onScroll={onScrollInBox}>
+        {/* <CardArray keyArray={dataArray} key="unique" /> */}
+        {/* <Card testKey="a"/> */}
+        {cardsTemp}
       </div>
+      <div className="sideBox">
+        <label>box width : {boxX}</label>
+        <label>box height x : {boxY}</label>
+        <label>scroll x : {catchX}</label>
+        <label>scroll y : {catchY}</label>
+        </div>
     </div>
   );
+
 }
