0d09266d1c5a34723afe48810ba3d4962da809a3
ichir0roieHome <ichir0roie.com>
2022-06-06 19:17:26 +0900

    add accessLib

---
+++ ui-test-windows/lib/accessDiary.ts
@@ -0,0 +1,28 @@
+import {Diary} from "./classes/Diary.ts"
+
+
+
+export class AccessDiary{
+
+    // for initialize.
+    public getRange(years:Array<number>,dateFrom:Date,dateTo:Date):Array<Array<Diary>>{
+        let data:Array<Array<Diary>>=[];
+
+        return data;
+    }
+
+    // concept functions.
+    public getYear(year:number,dateFrom:Date,dateTo:Date):Array<Diary>{
+        let data:Array<Diary>=[];
+
+        return data;
+    }
+
+    public getDaily(year:number,dayPosFrom:number,dayPosTo:number){
+
+    }
+
+    // functions conscious of RDB.
+    
+
+}
